1a056bae5aef3b6e926cdeb6a167c8959e450bd7
josh-boat365 <nyarko.boateng20@outlook.com>
2025-07-02 11:43:53 +0000

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,62 +1,65 @@
+<!-- JAVASCRIPT -->
+<script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+<script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+
+<!-- form repeater js -->
+<script src="{{ asset('assets/libs/jquery.repeater/jquery.repeater.min.js') }}"></script>
+
+<script src="{{ asset('assets/js/pages/form-repeater.int.js') }}"></script>
+
+<!-- jquery step -->
+<script src="{{ asset('assets/libs/jquery-steps/build/jquery.steps.min.js') }}"></script>
+
+<!-- form wizard init -->
+<script src="{{ asset('assets/js/pages/form-wizard.init.js') }}"></script>
+
+<!-- apexcharts -->
+<script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+
+<script src="{{ asset('assets/js/plugin.js') }}"></script>
+
+<!-- dashboard init -->
+<script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script>
+
+<!-- rating -->
+<script src="{{ asset('assets/libs/bootstrap-rating/bootstrap-rating.min.js') }}"></script>
+
+<script src="{{ asset('assets/js/pages/rating-init.js') }}"></script>
+
+<!-- Sweet Alerts js -->
+<script src="{{ asset('assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+
+<!-- Sweet alert init js-->
+<script src="{{ asset('assets/js/pages/sweet-alerts.init.js') }}"></script>
+
+<!-- App js -->
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
+
+
+<script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
+
+<script src="{{ asset('assets/libs/bootstrap-editable/js/index.js') }}"></script>
+<script src="{{ asset('assets/libs/moment/min/moment.min.js') }}"></script>
+
+<!-- Init js-->
+<script src="{{ asset('assets/js/pages/form-xeditable.init.js') }}"></script>
+
+
+<script>
+    $(document).ready(function() {
+        $('.select2').select2();
+    });
+</script>
+
+<script src="{{ asset('assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
+
+
 @push('scripts')
-    <!-- JAVASCRIPT -->
-    <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
-
-    <!-- form repeater js -->
-    <script src="{{ asset('assets/libs/jquery.repeater/jquery.repeater.min.js') }}"></script>
-
-    <script src="{{ asset('assets/js/pages/form-repeater.int.js') }}"></script>
-
-    <!-- jquery step -->
-    <script src="{{ asset('assets/libs/jquery-steps/build/jquery.steps.min.js') }}"></script>
-
-    <!-- form wizard init -->
-    <script src="{{ asset('assets/js/pages/form-wizard.init.js') }}"></script>
-
-    <!-- apexcharts -->
-    <script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-
-
-    <script src="{{ asset('assets/js/plugin.js') }}"></script>
-
-    <!-- dashboard init -->
-    <script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script>
-
-    <!-- rating -->
-    <script src="{{ asset('assets/libs/bootstrap-rating/bootstrap-rating.min.js') }}"></script>
-
-    <script src="{{ asset('assets/js/pages/rating-init.js') }}"></script>
-
-    <!-- Sweet Alerts js -->
-    <script src="{{ asset('assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
-
-    <!-- Sweet alert init js-->
-    <script src="{{ asset('assets/js/pages/sweet-alerts.init.js') }}"></script>
-
-    <!-- App js -->
-    <script src="{{ asset('assets/js/app.js') }}"></script>
-
-    <script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
-
-    <script src="{{ asset('assets/libs/bootstrap-editable/js/index.js') }}"></script>
-    <script src="{{ asset('assets/libs/moment/min/moment.min.js') }}"></script>
-
-    <!-- Init js-->
-    <script src="{{ asset('assets/js/pages/form-xeditable.init.js') }}"></script>
-
-
-    <script>
-        $(document).ready(function() {
-            $('.select2').select2();
-        });
-    </script>
-
-    <script src="{{ asset('assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
-
 
     <script src="https://cdn.tiny.cloud/1/ynbajxrf957pph9rrymxt50tc689r3r3ccj4iyfnlr7j0n6p/tinymce/7/tinymce.min.js"
         referrerpolicy="origin"></script>
@@ -73,4 +76,6 @@
             }
         });
     </script>
+
+
 @endpush
